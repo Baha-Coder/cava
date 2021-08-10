8b1113405a0f7346c65575e2003baf97c5f9d5ac
cavamodularautomation <88434635+cavamodularautomation@users.noreply.github.com>
2021-08-10 15:57:09 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php 
+
+$name = $_POST['name'];
+$adres = $_POST['address'];
+$tel = $_POST['tel'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
